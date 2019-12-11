32160adffbb42156c1ca89eb1735c4c8cca5ba68
prithviraj m pillai <prithvi_mp@yahoo.in>
2019-12-11 17:02:49 +0530

    Updates

---
+++ precache-manifest.06f72d84aeccaecdbe42f21455b609fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bb1f8eaf70c508b704ca9dec351379",
+    "url": "/monster-new/index.html"
+  },
+  {
+    "revision": "86b41955eb8b06306f2e",
+    "url": "/monster-new/static/css/main.5e877907.chunk.css"
+  },
+  {
+    "revision": "d706f28003964d78c4b2",
+    "url": "/monster-new/static/js/2.1339efe9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-new/static/js/2.1339efe9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86b41955eb8b06306f2e",
+    "url": "/monster-new/static/js/main.9e2525c3.chunk.js"
+  },
+  {
+    "revision": "beb19202446b6d8922fd",
+    "url": "/monster-new/static/js/runtime-main.75f80c34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-new/static/media/logo.5d5d9eef.svg"
+  }
+]);
